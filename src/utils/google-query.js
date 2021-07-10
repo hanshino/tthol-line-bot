b8090e4a34452481caae3c8971c8fc079a8e7df6
hanshino <c0952199517@gmail.com>
2021-07-10 18:08:45 +0800

    套件包裝

---
+++ src/utils/google-query.js
@@ -0,0 +1,54 @@
+const axios = require("axios").default;
+
+/**
+ * Fetch Google Sheet Data By SQL
+ * @param {Object}  params
+ * @param {String}  params.gid  Google Sheet tab id
+ * @param {String}  params.type Fetch Type
+ * - json
+ * - html
+ * @param {String}  params.query  SQL statement
+ * @param {String}  params.key  Google Sheet Key
+ * @return {Promise<Array>}
+ */
+module.exports = params => {
+  let { gid, type, query, key } = params;
+  let queryString = new URLSearchParams();
+
+  queryString.set("gid", gid);
+  queryString.set("tqx", `out:${type}`);
+  queryString.set("tq", query);
+
+  let url = `https://docs.google.com/spreadsheets/u/0/d/${key}/gviz/tq?${queryString.toString()}`;
+
+  return axios
+    .get(url)
+    .then(res => res.data)
+    .then(res => res.match(/\{.*\}/)[0])
+    .then(data => {
+      try {
+        return queryParser(JSON.parse(data));
+      } catch (e) {
+        console.log(data);
+        return null;
+      }
+    });
+};
+
+function queryParser(data) {
+  let rows = data.table.rows;
+  let title = data.table.cols.map(col => (col.label !== "" ? col.label.trim() : col.id));
+
+  let result = [];
+
+  rows.forEach(row => {
+    let temp = {};
+    row.c.forEach((value, index) => {
+      if (value === null) return;
+      temp[title[index]] = value.f || value.v;
+    });
+    result.push(temp);
+  });
+
+  return result;
+}
