f0c1d611b29b5bb1a3fc3a34408a4a1bb7338c75
hanshino <c0952199517@gmail.com>
2021-07-18 00:12:49 +0800

    路由調整

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { router, text } = require("bottender/router");
+const { router, route } = require("bottender/router");
 const routes = [
   ...require("./app/controllers/templateController").routes,
   ...require("./app/controllers/manualController").routes,
@@ -6,12 +6,19 @@ const routes = [
   ...require("./app/controllers/monsterController").routes,
   ...require("./app/controllers/magicController").routes,
   ...require("./app/controllers/itemController").routes,
-  // text("*", context => context.sendText("沒有符合的指令")),
+  ...require("./app/controllers/advanceController").routes,
+  route("*", fallback),
 ];
+
+function fallback(context) {
+  if (context.event.source.type === "user") {
+    context.sendText("沒有符合的指令");
+  }
+}
 
 module.exports = async function App(context) {
   let { userId, type } = context.event.source;
   let { text } = context.event.message;
-  console.log(userId, text);
+  console.log(userId, context.event.source[`${type}Id`], text);
   return router(routes);
 };
