d2f54436843ec133a32f67fc24bdb09646fd5fc4
hanshino <c0952199517@gmail.com>
2021-07-19 22:47:55 +0800

    錯誤修正

--- src/app/models/item.js
+++ src/app/models/item.js
@@ -1,4 +1,4 @@
-const sqlite = require("../../utils/sqlite")("tthol.sqlite");
+const sqlite = require("../../utils/sqlite")(process.env.TTHOL_DATABASE);
 const table = "items";
 
 module.exports = () => sqlite(table);
